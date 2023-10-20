3d964808f4cf2d34bb69b543f6c1a59e753a0cc4
Kartik9082 <kashyap.kartik2000@gmail.com>
2023-10-20 18:37:22 +0530

    initial

--- src/components/hooks/useRestaurantList.js
+++ src/components/hooks/useRestaurantList.js
@@ -1,5 +1,27 @@
-const useRestaurantList = () =>{
+import { useState, useEffect } from "react";
+import { SWIGGY_API } from "../../utils/constants";
 
+const useRestaurantList = () =>{
+    const [allRestaurants, setAllRestaurants] = useState([]);
+    const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+
+    useEffect(() => {
+        getRestaurant();
+      }, []);
+    
+      async function getRestaurant() {
+        const data = await fetch(SWIGGY_API);
+        const JSON = await data.json();
+        // console.log(JSON);
+        setAllRestaurants(
+          JSON?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+        );
+        setFilteredRestaurants(
+          JSON?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+        );
+      }
+
+    return [allRestaurants, filteredRestaurants, setFilteredRestaurants, setAllRestaurants];
 }
 
 export default useRestaurantList;
